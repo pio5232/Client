b021bb3952c349687447e02eebc34ae67892e798
pio5232 <prioewoe@naver.com>
2024-11-14 21:47:36 +0900

    client에서 통신을 위한 작업 진행중

--- OmokClient/Form1.Designer.cs
+++ OmokClient/Form1.Designer.cs
@@ -28,43 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.enterButton = new System.Windows.Forms.Button();
+            this.ipTB = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.portTB = new System.Windows.Forms.TextBox();
+            this.roomListCB = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.connTB = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.connButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.chattingRoom = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // enterButton
             // 
-            this.button1.Location = new System.Drawing.Point(530, 844);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(398, 234);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.enterButton.Location = new System.Drawing.Point(248, 771);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(261, 67);
+            this.enterButton.TabIndex = 0;
+            this.enterButton.Text = "입장 버튼";
+            this.enterButton.UseVisualStyleBackColor = true;
+            this.enterButton.Click += new System.EventHandler(this.enterButton_Click);
             // 
-            // textBox1
+            // ipTB
             // 
-            this.textBox1.Location = new System.Drawing.Point(488, 334);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 35);
-            this.textBox1.TabIndex = 1;
+            this.ipTB.Location = new System.Drawing.Point(248, 27);
+            this.ipTB.Name = "ipTB";
+            this.ipTB.Size = new System.Drawing.Size(261, 32);
+            this.ipTB.TabIndex = 1;
+            this.ipTB.Text = "127.0.0.1";
+            this.ipTB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(1238, 208);
+            this.label1.Location = new System.Drawing.Point(12, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.Size = new System.Drawing.Size(212, 34);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.label1.Text = "ip";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(212, 32);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "port";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // portTB
+            // 
+            this.portTB.Location = new System.Drawing.Point(248, 87);
+            this.portTB.Name = "portTB";
+            this.portTB.Size = new System.Drawing.Size(261, 32);
+            this.portTB.TabIndex = 3;
+            this.portTB.Text = "6000";
+            this.portTB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // roomListCB
+            // 
+            this.roomListCB.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple;
+            this.roomListCB.FormattingEnabled = true;
+            this.roomListCB.Location = new System.Drawing.Point(248, 353);
+            this.roomListCB.Name = "roomListCB";
+            this.roomListCB.Size = new System.Drawing.Size(261, 392);
+            this.roomListCB.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(12, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(212, 35);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "연결 상태";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // connTB
+            // 
+            this.connTB.Location = new System.Drawing.Point(248, 150);
+            this.connTB.Name = "connTB";
+            this.connTB.Size = new System.Drawing.Size(261, 32);
+            this.connTB.TabIndex = 6;
+            this.connTB.Text = "연결 안 됨";
+            this.connTB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(12, 353);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(212, 61);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "방 정보";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // connButton
+            // 
+            this.connButton.Location = new System.Drawing.Point(248, 210);
+            this.connButton.Name = "connButton";
+            this.connButton.Size = new System.Drawing.Size(261, 67);
+            this.connButton.TabIndex = 9;
+            this.connButton.Text = "연결 버튼";
+            this.connButton.UseVisualStyleBackColor = true;
+            this.connButton.Click += new System.EventHandler(this.connButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(556, 160);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(557, 794);
+            this.textBox1.TabIndex = 10;
+            // 
+            // chattingRoom
+            // 
+            this.chattingRoom.Font = new System.Drawing.Font("굴림", 12F);
+            this.chattingRoom.Location = new System.Drawing.Point(550, 45);
+            this.chattingRoom.Name = "chattingRoom";
+            this.chattingRoom.Size = new System.Drawing.Size(563, 62);
+            this.chattingRoom.TabIndex = 11;
+            this.chattingRoom.Text = "채팅창";
+            this.chattingRoom.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 26F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1534, 1028);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(1174, 1032);
+            this.Controls.Add(this.chattingRoom);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.connButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.connTB);
+            this.Controls.Add(this.roomListCB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.portTB);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ipTB);
+            this.Controls.Add(this.enterButton);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
@@ -75,9 +180,18 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button enterButton;
+        private System.Windows.Forms.TextBox ipTB;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox portTB;
+        private System.Windows.Forms.ComboBox roomListCB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox connTB;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button connButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label chattingRoom;
     }
 }
 
--- OmokClient/Form1.cs
+++ OmokClient/Form1.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Net;
+using System.Net.Mime;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,27 +18,158 @@ namespace OmokClient
     {
         private readonly C_Network.RefChattingClient client;
 
-        //public unsafe delegate RefErrorCode CallbackDelegate(C_Utility::RefSerializationBuffer^);
-
-        // Dictionary<RefPacketType, CallbackDelegate> callbackDic = new Dictionary<RefPacketType, CallbackDelegate>();
-
+        Dictionary<RefPacketType, Func<RefErrorCode>> MakeFuncsDic =
+            new Dictionary<RefPacketType, Func<RefErrorCode>>();
+        public enum ConnectionType
+        {
+            CONN,
+            DISCONN,
+        }
+        ConnectionType curConnType = ConnectionType.CONN;
         public Form1()
         {
-            client = new C_Network.RefChattingClient("127.0.0.1", 6000,1);
-
-            //RegistHandle();
-
             InitializeComponent();
+
+            string ip = ipTB.Text;
+            ushort port = ushort.Parse(portTB.Text);
+            client = new C_Network.RefChattingClient(ip,port,1);
+
+            RegisterFunctions();
         }
 
-        public void RegistHandle(RefPacketType packetType, C_Utility.RefSerializationBuffer refBuffer)
+        public void RegisterFunctions()
         {
-            client.RegisterHandler(RefPacketType.LOG_IN_RESPONSE_PACKET, Func);
+            client.RegisterHandler(RefPacketType.LOG_IN_RESPONSE_PACKET, ProcessLogIn);
+            client.RegisterHandler(RefPacketType.ROOM_LIST_RESPONSE_PACKET, ProcessRoomInfo);
+            client.RegisterHandler(RefPacketType.ENTER_ROOM_RESPONSE_PACKET, ProcessEnterRoom);
+            client.RegisterHandler(RefPacketType.ENTER_ROOM_NOTIFY_PACKET, ProcessNotifyEnterRoom);
+            client.RegisterHandler(RefPacketType.LEAVE_ROOM_RESPONSE_PACKET, ProcessLeaveRoom);
+            client.RegisterHandler(RefPacketType.LEAVE_ROOM_NOTIFY_PACKET, ProcessNotifyLeaveRoom);
+            client.RegisterHandler(RefPacketType.CHAT_NOTIFY_PACKET, ProcessChatNotify);
+            client.RegisterHandler(RefPacketType.CHAT_TO_USER_RESPONSE_PACKET, ProcessChatUser);
+            client.RegisterHandler(RefPacketType.CHAT_TO_ROOM_RESPONSE_PACKET, ProcessChatRoom);
         
         }
-        C_Network.RefErrorCode Func(C_Utility.RefSerializationBuffer buff)
+
+        private void enterButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // 받은 메시지를 처리하는 부분.
+        #region 패킷처리
+        C_Network.RefErrorCode ProcessLogIn(C_Utility.RefRecvSerializationBuffer buff)
+        {
+            connTB.Text = "연결됨";
+
+            
+            return RefErrorCode.NONE;
+        }
+
+        C_Network.RefErrorCode ProcessRoomInfo(C_Utility.RefRecvSerializationBuffer buff)
         {
             return RefErrorCode.NONE;
+        }
+
+        C_Network.RefErrorCode ProcessEnterRoom(C_Utility.RefRecvSerializationBuffer buff)
+        {
+            return RefErrorCode.NONE;
+        }
+        C_Network.RefErrorCode ProcessNotifyEnterRoom(C_Utility.RefRecvSerializationBuffer buff)
+        {
+            return RefErrorCode.NONE;
+        }
+        C_Network.RefErrorCode ProcessLeaveRoom(C_Utility.RefRecvSerializationBuffer buff)
+        {
+            return RefErrorCode.NONE;
+        }
+        C_Network.RefErrorCode ProcessNotifyLeaveRoom(C_Utility.RefRecvSerializationBuffer buff)
+        {
+            int a;
+            buff.GetBuffer() >> a;
+            return RefErrorCode.NONE;
+        }
+        C_Network.RefErrorCode ProcessChatNotify(C_Utility.RefRecvSerializationBuffer buff)
+        {
+            return RefErrorCode.NONE;
+        }
+        C_Network.RefErrorCode ProcessChatRoom(C_Utility.RefRecvSerializationBuffer buff)
+        {
+            return RefErrorCode.NONE;
+        }
+        C_Network.RefErrorCode ProcessChatUser(C_Utility.RefRecvSerializationBuffer buff)
+        {
+            return RefErrorCode.NONE;
+        }
+
+
+        // 보낼 메시지를 등록하는 부분.
+        C_Network.RefErrorCode SendMessage(C_Network.RefPacketType packetType)
+        {
+            C_Network.RefErrorCode error = MakeFuncsDic[packetType]();
+
+            return error;
+        }
+
+        C_Network.RefErrorCode MakeLogInRequestPacket()
+        {
+
+            C_Network.RefLogInRequestPacket packet;
+
+            uint size = sizeof(RefLogInRequestPacket);
+            C_Utility.RefSendSerializationBuffer sendBuffer = client.MakeBuffer();
+
+            ulong id = 1;
+            
+            ulong pw = 2;
+
+            sendBuffer.Attach(id).Attach(pw); 
+            
+            return C_Network.RefErrorCode.NONE;
+        }
+
+        C_Network.RefErrorCode MakeRoomListRequestPacket()
+        {
+            return C_Network.RefErrorCode.NONE;
+        }
+        C_Network.RefErrorCode MakeEnterRoomRequestPacket()
+        {
+            return C_Network.RefErrorCode.NONE;
+        }
+        C_Network.RefErrorCode MakeLeaveRoomRequestPacket()
+        {
+            return C_Network.RefErrorCode.NONE;
+        }
+        C_Network.RefErrorCode MakeChatToUserRequestPacket()
+        {
+            return C_Network.RefErrorCode.NONE;
+        }
+        C_Network.RefErrorCode MakeChatToRoomRequestPacket()
+        {
+            return C_Network.RefErrorCode.NONE;
+        }
+        #endregion
+
+        private void connButton_Click(object sender, EventArgs e)
+        {
+            if(curConnType == ConnectionType.DISCONN)
+            {
+                client.Begin();
+
+                SendMessage(C_Network.RefPacketType.LOG_IN_REQUEST_PACKET);
+                
+                connButton.Text = "연결 끊기 버튼";
+                
+                curConnType = ConnectionType.CONN;
+            }
+            else
+            {
+                client.End();
+
+                connButton.Text = "연결 버튼";
+
+                curConnType = ConnectionType.DISCONN;
+            }
         }
     }
 }
